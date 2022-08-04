c81098af2a08dc71191d5ce5bc867e71f046572b
nelly <nrabubakirova@gmail.com>
2022-08-04 22:56:24 +0600

    Second version 1. Background color gets changed with every circle 2. There are circles and squares altogether

--- Rainbow.py
+++ Rainbow.py
@@ -2,18 +2,26 @@ import turtle
 from itertools import cycle
 
 colors = cycle(['red', 'orange', 'yellow', 'green', 'blue', 'purple'])
-def draw_circle(size, angle, shift):
+def draw_shape(size, angle, shift, shape):
     turtle.bgcolor(next(colors))
     turtle.pencolor(next(colors))
-    turtle.circle(size)
+    next_shape = ''
+    if shape == 'circle':
+        turtle.circle(size)
+        next_shape = 'square'
+    elif shape == 'square':
+        for i in range(4):
+            turtle.forward(size * 2)
+            turtle.left(90)
+        next_shape = 'circle'
     turtle.right(angle)
     turtle.forward(shift)
-    draw_circle(size, angle+1, shift+1)
+    draw_shape(size + 5, angle + 1, shift + 1, next_shape)
 
 
 turtle.speed('fast')
-turtle.pensize(8)
+turtle.pensize(4)
 
-draw_circle(30, 10, 1)
+draw_shape(30, 10, 1, 'circle')
 
 
