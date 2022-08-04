b99120bd5dd55ecfca044c69807116815c4a3d5a
nelly <nrabubakirova@gmail.com>
2022-08-04 22:45:35 +0600

    First version

---
+++ Rainbow.py
@@ -0,0 +1,18 @@
+import turtle
+from itertools import cycle
+
+colors = cycle(['red', 'orange', 'yellow', 'green', 'blue', 'purple'])
+def draw_circle(size, angle, shift):
+    turtle.pencolor(next(colors))
+    turtle.circle(size)
+    turtle.right(angle)
+    turtle.forward(shift)
+    draw_circle(size, angle+1, shift)
+
+turtle.bgcolor('black')
+turtle.speed('fast')
+turtle.pensize(8)
+
+draw_circle(30, 0, 1)
+
+
